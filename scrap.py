decddf912d3636254654719fb263987414087e8a
luizvmachado <97991563+luizvmachado@users.noreply.github.com>
2022-02-22 17:58:32 -0300

    Add files via upload

---
+++ scrap.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+lista_descricoes = []
+if __name__ == '__main__':
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(10)
+    driver.maximize_window()
+    driver.get('https://www.linkedin.com/jobs/ci%C3%AAncia-de-dados-vagas/?originalSubdomain=br')
+    resultados = driver.find_elements(By.CLASS_NAME, 'base-card')
+    while True:
+        for r in resultados[len(lista_descricoes):]:
+            r.click()
+            time.sleep(4)
+            try:
+                descricao = driver.find_elements(By.CLASS_NAME, 'description')
+                lista_descricoes.append(descricao[0].text)
+            except:
+                print('Erro')
+                pass
+        resultados = driver.find_elements(By.CLASS_NAME, 'base-card')
+        if len(lista_descricoes) == len(resultados):
+            break
+    descricao_salvar = '\n'.join(lista_descricoes)
+    with open('descricoes_vagas.txt','w') as f:
+        f.write(descricao_salvar)
+    driver.quit()
